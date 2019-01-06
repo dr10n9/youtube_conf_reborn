e91bd23f3e8e2d2598e7dac67d88adb1ad2015f5
dimas <destaom@gmail.com>
2019-01-07 01:57:02 +0200

    Setup

---
+++ components/Welcome.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="jumbotron">
+        <h1>Youtube Conf Reborn</h1>
+        <p>Мертворожденный и ныне перерожденный</p>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <Navbar/>
+    <Welcome/>
+  </div>
+</template>
+
+<script>
+
+import Welcome from '~/components/Welcome.vue'
+import Navbar from '~/components/Navbar'
+
+export default {
+  components: {
+    Welcome,
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
